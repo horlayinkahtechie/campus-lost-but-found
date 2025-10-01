5ecdf0011b15d2adc7296eedf7f638c909fe1516
horlayinkahtechie <horlayinkah2005@gmail.com>
2025-10-01 20:05:02 +0100

    Error fixed

--- app/report-found-item/report-found-item.js
+++ app/report-found-item/report-found-item.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import { supabase } from "../lib/supabaseClient";
 import Link from "next/link";
